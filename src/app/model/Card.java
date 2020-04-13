2d49e28d37eeb1578fc6e7e7b037fa864eed9c20
StasinosStylianos <sty.stasinoss@gmail.com>
2020-04-13 22:17:32 +0300

    Add card model

---
+++ src/app/model/Card.java
@@ -0,0 +1,11 @@
+package app.model;
+
+import java.util.Date;
+
+public class Card {
+    private String type;
+    private String Cardnum;
+    private String Carduser;
+    private Date exp_date;
+    private String cvc;
+}
